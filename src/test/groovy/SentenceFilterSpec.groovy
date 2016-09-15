4c074a86f4225e5fa42e2e864cf9c3a19c85cf8e
Hanif Fakira <fakira.work@gmail.com>
2016-09-15 14:15:54 +0100

    5th test: no vowels in 1 word

--- src/test/groovy/SentenceFilterSpec.groovy
+++ src/test/groovy/SentenceFilterSpec.groovy
@@ -3,36 +3,12 @@ import spock.lang.*
 class SentenceFilterSpec extends Specification {
   def sentence_filter = new SentenceFilter()
 
-  def 'replace 1 vowel in 1 word sentence'() {
-    when:
-    String result = sentence_filter.change("red")
-
-    then:
-    result == 'r-d'
-  }
-
-  def 'replace 2 same vowels in 1 word sentence'() {
-    when:
-    String result = sentence_filter.change("green")
-
-    then:
-    result == 'gr--n'
-  }
-
-  def 'replace 3 different vowels in 1 word sentence'() {
-    when:
-    String result = sentence_filter.change("orange")
-
-    then:
-    result == '-r-ng-'
-  }
-
   def 'replace n different vowels in 1 word sentence'() {
     when:
-    String result = sentence_filter.change(word)
+    String result = sentence_filter.change(word);
 
     then:
-    result == expected
+    result == expected;
 
     where:
     word      | expected
@@ -43,7 +19,12 @@ class SentenceFilterSpec extends Specification {
     'purple'  | 'p-rpl-'
   }
 
-  // test rest of vowels i and u
-  // test no vowels in word
+  def 'no replacement when no vowels in word sentence'() {
+    when:
+    String result = sentence_filter.change('rhythm');
+
+    then:
+    result == 'rhythm';
+  }
 
 }
