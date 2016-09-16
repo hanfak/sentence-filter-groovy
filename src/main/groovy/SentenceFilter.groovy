c9a71d4536e14d6ae3f171c268dafea44b5038bf
Hanif Fakira <fakira.work@gmail.com>
2016-09-16 10:01:37 +0100

    refactored to meth logic and exception words

--- src/main/groovy/SentenceFilter.groovy
+++ src/main/groovy/SentenceFilter.groovy
@@ -1,17 +1,28 @@
 class SentenceFilter {
   def change(String sentence) {
     String[] list = sentence.split(" ");
-    String[] exceptions = ["coloured","covered"];
     this.getBannedWords().each {banned_word ->
         list = list.collect {word ->
-          word.toLowerCase().contains(banned_word) &&  !exceptions.findAll {it.toLowerCase() == word} ? this.replaceVowels(word) : word;
+          this.checkWordContainsBannedWord(word, banned_word) &&  this.checkWordNotInExceptions(word) ? this.replaceVowels(word) : word;
         }
     }
     return list.join(' ')
   }
 
+  private def getExceptionWords() {
+    return ["coloured","covered"];
+  }
+
+  private def checkWordNotInExceptions(word) {
+    !this.getExceptionWords().findAll {it.toLowerCase() == word}
+  }
+
   private def getBannedWords() {
     return ["orange", 'red', 'green']
+  }
+
+  private def checkWordContainsBannedWord(word, banned_word) {
+    return word.toLowerCase().contains(banned_word)
   }
 
   private def replaceVowels(String word){
